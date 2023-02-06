f405755fdee642f3e24ca5fb761fc84daae9c9b6
Maryia Piarshai <mary.pershay@gmail.com>
2023-02-06 16:37:16 +0300

    feat: add en-ru swithcer

--- src/app/common/app.header.ts
+++ src/app/common/app.header.ts
@@ -1,20 +1,68 @@
 import { ComponentConfig } from '../../framework/tools/interfaces';
-import { DMComponent } from '../../framework/index';
+import { DMComponent, router } from '../../framework/index';
 import { closeMenu, toggleMenu } from '../service/controlMenu';
 import { switchingOnLoad, switchThemeOnClick } from '../service/switchThemes';
+import { checkLang } from '../service/lang.service';
+import { cssComponentMenuComponent } from '../components/css-component-menu.component';
 
 
 export class AppHeader extends DMComponent {
   constructor(config: ComponentConfig) {
     super(config);
     switchingOnLoad();
+    this.createContent();
   }
 
   public isMenuOpen = false;
 
+  public createContent() {
+    const { lang, savedLang } = checkLang();
+    const activeClassEn: string = savedLang === 'en' ? 'language-switcher-item--active' : '';
+    const activeClassRu: string = savedLang === 'ru' ? 'language-switcher-item--active' : '';
+    this.template = `
+      <header class="page__container header">
+            <div class="header__brand">
+              <h1 class="header__title">
+                <a class="header__link" href="/#">
+                  style<span class="header__subtitle">It</span>
+                </a>
+              </h1>
+            </div>
+
+            <button class="menu__button" type="button" aria-expanded="false" aria-label="toggle menu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <div class="header__menu">
+              
+              <nav role="navigation" aria-label="Main navigation">
+                <ul class="menu__list">
+                  <li><a href="#css-generator" class="menu-item link">${lang.cssGenerator}</a></li>
+                  <li><a href="#card-generator" class="menu-item link">${lang.cardGenerator}</a></li>
+                  <li><a href="#game" class="menu-item link">${lang.game}</a></li>
+                  <li><a href="#about" class="menu-item link">${lang.aboutPage}</a></li>
+                </ul>
+              </nav>    
+            
+              <div class="header__buttons">           
+                <ul class="language-switcher" aria-label="language switcher">
+                  <li><button class="language-switcher-item ${activeClassEn}" tabindex="0" aria-label="english">Eng</button></li>
+                  <li><button class="language-switcher-item ${activeClassRu}" tabindex="0" aria-label="russian">Rus</button></li>               
+                </ul>     
+                <button class="theme-switcher" aria-label="theme switcher" aria-live="polite">
+                  <img src="./light.svg" alt="" width="30" height="30">                
+                </button>
+              </div>
+            </div>      
+          </header>
+    `;
+  }
+
   private events(): Record<string, string> {
     return {
-      'click .header': 'handleControlClicks',     
+      'click .header': 'handleControlClicks',
+      'click .language-switcher': 'changeLang',
     };
   }
 
@@ -29,51 +77,27 @@ export class AppHeader extends DMComponent {
     }
   }
 
+  private changeLang(event: Event): void {
+    const target = event.target as HTMLElement;
+    const langSwitcherText = target.getAttribute('aria-label');
+    if (langSwitcherText === 'english') {
+      localStorage.setItem('lang', 'en');
+    } else {
+      localStorage.setItem('lang', 'ru');
+    }
+    this.createContent();
+    this.render();
+    if (router.getUrl() === 'css-generator') {
+      cssComponentMenuComponent.createContent();
+      cssComponentMenuComponent.render();
+    }
+
+  }
 
 }
 
 export const appHeader = new AppHeader({
   selector: 'app-header',
-  template: `
-          <header class="page__container header">
-            <div class="header__brand">
-              <h1 class="header__title">
-                <a class="header__link" href="/#">
-                  style<span class="header__subtitle">It</span>
-                </a>
-              </h1>
-            </div>
-
-            <button class="menu__button" type="button" aria-expanded="false" aria-label="toggle menu">
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-
-            <div class="header__menu">
-              
-              <nav role="navigation" aria-label="Main navigation">
-                <ul class="menu__list">
-                  <li><a href="#css-generator" class="menu-item link">CSS Generator</a></li>
-                  <li><a href="#card-generator" class="menu-item link">Card Generator</a></li>
-                  <li><a href="#game" class="menu-item link">Game</a></li>
-                  <li><a href="#about" class="menu-item link">About</a></li>
-                </ul>
-              </nav>    
-            
-              <div class="header__buttons">           
-                <ul class="language-switcher" aria-label="language switcher">
-                  <li><button class="language-switcher-item" tabindex="0" aria-label="english">Eng</button></li>
-                  <li><button class="language-switcher-item language-switcher-item--active" tabindex="0" aria-label="russian">Rus</button></li>               
-                </ul>     
-                <button class="theme-switcher" aria-label="theme switcher" aria-live="polite">
-                  <img src="./light.svg" alt="" width="30" height="30">                
-                </button>
-              </div>
-
-            </div>      
-
-          </header>
-    `,
+  template: '',
   childComponents: [],
 });
