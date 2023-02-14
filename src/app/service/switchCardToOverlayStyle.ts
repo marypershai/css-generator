ddbf9261fe7f880cbc77206dc1adb001e8b8c76c
xxx <25452644+bartoshevich@users.noreply.github.com>
2023-02-14 11:24:07 +0300

    refactor: fine-tune shadow properties for header__menu--open block

--- src/app/service/switchCardToOverlayStyle.ts
+++ src/app/service/switchCardToOverlayStyle.ts
@@ -7,6 +7,7 @@ function setDefaultProperties(): void {
   const html = document.firstElementChild as HTMLElement;
   html.style.setProperty('--align-items-value', 'flex-start');
   html.style.setProperty('--text-align-value', 'start');
+  html.style.setProperty('--justify-content-value', 'flex-end');
   html.style.setProperty('--overlay-direction', '0deg');
   html.style.setProperty('--card-text-shadow', '1px 1px 5px rgba(0, 0, 0, 0.5)');
 }
